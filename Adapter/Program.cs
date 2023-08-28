581c12e0a297dafe08f3867a97b1106aa5f15ce2
Mohamed Riyad <mohamed.riyad@advaeg.com>
2023-08-29 00:13:02 +0300

    Added new adapter pattern.
    - Changed decorator project name.
    - Changed proxy project name.

---
+++ Adapter/Program.cs
@@ -0,0 +1,8 @@
+﻿using Adapter;
+
+var employee = new Employee { basicSalary = 1000 };
+var machineOperator = new MachineOperator { basicSalary = 2000 };
+var salaryCalculator = new SalaryAdapter();
+
+var salary = salaryCalculator.CalcSalary(machineOperator);
+Console.WriteLine($"Total salary : {salary}");
