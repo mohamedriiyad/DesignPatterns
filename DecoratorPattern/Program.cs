8e2d18907c3230abe430b06d467ad97f2515f946
Mohamed Riyad <mohamed.riyad@advaeg.com>
2023-08-28 22:31:02 +0300

    Added new decorator pattern.

---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,6 @@
+﻿using DecoratorPattern;
+
+var smsService = new ConcreteSMSService();
+var notificationEmailService = new NotificationEmailDecorator();
+notificationEmailService.SetService(smsService);
+Console.WriteLine(notificationEmailService.SendSMS("6521", "0123456252", "Message content"));
