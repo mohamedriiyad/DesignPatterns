e86a9c94f1dfb1ba7111301d6e39f1ec8a6fbee9
Mohamed Riyad <mohamed.riyad@advaeg.com>
2023-09-10 23:57:37 +0300

    Added new memento pattern.

---
+++ Memento/Program.cs
@@ -0,0 +1,15 @@
+﻿using Memento;
+
+var order = new Order { Id = 1, Name = "Order name" };
+var orderMemento = order.SaveStateToMemento();
+
+var careTaker = new CareTaker();
+Console.WriteLine(careTaker.AddMemento(orderMemento));
+order = new Order { Id = 2, Name = "Order name1" };
+orderMemento = order.SaveStateToMemento();
+Console.WriteLine(careTaker.AddMemento(orderMemento));
+
+order.RestoreStateFromMemento(careTaker.GetOrderMemento(0));
+Console.WriteLine(order.Name);
+order.RestoreStateFromMemento(careTaker.GetOrderMemento(1));
+Console.WriteLine(order.Name);
